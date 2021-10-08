38a423147b9c3decc9382661df5c9e08132b8625
apate475 <91709166+apate475@users.noreply.github.com>
2021-10-08 11:46:53 -0500

    Update DNA_project_Milestone_1_functions.py

--- DNA_project_Milestone_1_functions.py
+++ DNA_project_Milestone_1_functions.py
@@ -54,3 +54,110 @@ if reverse_compliment("AAAACCCGGT") == "ACCGGGTTTT":
     print('True')
 else:
     print('False')
+"""
+@author: Adesh Patel
+"""
+def rna2codon(rna):
+  protein_string = ""
+  string_length = len(rna)
+  for i in range(int((string_length - string_length % 3) / 3)):
+    index = i * 3
+    if (rna[index] == 'U'):
+      if (rna[index + 1] == 'U'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C'):
+          protein_string += 'F'
+        elif (rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'L'
+
+      elif (rna[index + 1] == 'C'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'S'
+
+      elif (rna[index + 1] == 'A'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C'):
+          protein_string += 'Y'
+
+      elif (rna[index + 1] == 'G'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C'):
+          protein_string += 'C'
+
+        elif (rna[index + 2] == 'G'):
+          protein_string += 'W'
+
+    elif (rna[index] == 'C'):
+      if (rna[index + 1] == 'U'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'L'
+
+      elif (rna[index + 1] == 'C'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'P'
+
+      elif (rna[index + 1] == 'A'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C'):
+          protein_string += 'H'
+
+        elif (rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'Q'
+
+      elif (rna[index + 1] == 'G'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'R'
+
+    elif (rna[index] == 'A'):
+      if (rna[index + 1] == 'U'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A'):
+          protein_string += 'I'
+
+        elif (rna[index + 2] == 'G'):
+          protein_string += 'M'
+
+      elif (rna[index + 1] == 'C'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'T'
+
+      elif (rna[index + 1] == 'A'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C'):
+          protein_string += 'N'
+
+        elif (rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'K'
+
+      elif (rna[index + 1] == 'G'):
+        if (rna[index + 2] == 'U'or rna[index + 2] == 'C'):
+          protein_string += 'S'
+
+        elif (rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'R'
+
+    elif (rna[index] == 'G'):
+      if (rna[index + 1] == 'U'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'V'
+
+      elif (rna[index + 1] == 'C'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'A'
+
+      elif (rna[index + 1] == 'A'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C'):
+          protein_string += 'D'
+
+        elif (rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'E'
+
+      elif (rna[index + 1] == 'G'):
+        if (rna[index + 2] == 'U' or rna[index + 2] == 'C' or 
+            rna[index + 2] == 'A' or rna[index + 2] == 'G'):
+          protein_string += 'G'
+    
+  return protein_string;
+
