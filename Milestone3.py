9d042fe5d480968ec6a8d825741e983bb90eed35
Logington <91852837+Logington@users.noreply.github.com>
2021-11-29 12:23:34 -0600

    Update Milestone3.py

--- Milestone3.py
+++ Milestone3.py
@@ -1,1 +1,13 @@
-
+def assemble_genome(dna_list):
+    length = len(dna_list)
+    combine = [[0 for _ in range(length)] for _ in range(length)]
+    for a in range(length):
+        for b in range(length):
+            if a == b:
+                continue
+            c, d = dna_list[a], dna_list[b]
+            letters = len(c)
+            for z in range(1, letters):
+                if d.startswith(c[z:]):
+                    combine[a][b] = letters - z
+                    break
