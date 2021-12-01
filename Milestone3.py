807f876cfddea306da6d3954724d563455cde1cd
Logington <91852837+Logington@users.noreply.github.com>
2021-12-01 12:12:10 -0600

    Update Milestone3.py

--- Milestone3.py
+++ Milestone3.py
@@ -1,17 +1,7 @@
-def assemble_genome(dna_list):
-    length = len(dna_list)
-    combine = [[0 for _ in range(length)] for _ in range(length)]
-    for a in range(length):
-        for b in range(length):
-            if a == b:
-                continue
-            c, d = dna_list[a], dna_list[b]
-            letters = len(c)
-            for z in range(1, letters):
-                if d.startswith(c[z:]):
-                    combine[a][b] = letters - z
-                    break
-
+def load_file(file):
+    myfile = open( 'file' )
+    mydata = myfile.readlines()
+    myfile.close()
 
 def assemble_genome2(dna_list):
   mammoth = dna_list[0]
@@ -29,11 +19,8 @@ def assemble_genome2(dna_list):
     print(mammoth)
     print(len(mammoth))
   return mammoth
-    
 
-f = open('ms3-dna-100.txt', 'r')
-
-print(assemble_genome2(f))
+print(assemble_genome2(load_file('ms3-dna-mammuthus.txt')))
     
 
 
